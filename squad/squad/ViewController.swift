7471d09b5b3f776086335dcc0d171959c171a00d
Grayson Wise <gkw@duke.edu>
2017-10-31 21:42:23 -0400

    initial commit

---
+++ squad/squad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  squad
+//
+//  Created by Grayson Wise on 10/31/17.
+//  Copyright © 2017 Grayson Wise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
